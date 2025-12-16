559144a6de5c36c31462a18d7feac1ce10efad11
avishkaravhad <avishkaravhad87@gmail.com>
2025-12-16 09:39:22 +0000

    this is onemore updated but wrong block

--- s3main.tf
+++ s3main.tf
@@ -5,10 +5,18 @@ provider "aws" {
 }
 resource "aws_s3_bucket" "my_bucket" {
     bucket = "my-unique-bucket-name-12345" # Replace with a unique bucket name
-    acl    = "private"
+    acl   = "private"
 
     tags = {
         Name        = "MyBucket"
         Environment = "Dev"
     }
 }
+#adding object to s3 bucket
+resource "aws_s3_bucket_object" "my_object" {
+    bucket = "aws_s3_bucket.my_bucket.id"
+    key    = "myfile.txt"
+    source = "myfile.txt"   
+
+    etag   = filemd5("./myfile.txt")
+}    
